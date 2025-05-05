90f5dad908b72678fd236c831ac0d1ae7f89a612
Ikponmwosa Ajayi <82876941+Ajayi108@users.noreply.github.com>
2025-05-05 22:20:45 +0200

    Ajayi

--- src/pages/CalculatorPage/CalculatorPage.jsx
+++ src/pages/CalculatorPage/CalculatorPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "../../components/Navbar/Navbar";
+import './CalculatorPage.css';
 
 export default function CalculatorPage() {
   const [formData, setFormData] = useState({
