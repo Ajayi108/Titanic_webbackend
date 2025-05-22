2b3a60a85c92d19381fce598273052f90928fb9a
Edward Samlafo-Adams <edwardmawuko75@gmail.com>
2025-05-22 21:09:05 +0200

    layout fix

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,11 +1,12 @@
+import "./Contact.css";
+
 function Contact() {
-    return (
-      <div className="page-container">
-        <h1>Contact Info</h1>
-        <p>Reach us at: icebergai@titanic.com</p>
-      </div>
-    );
-  }
-  
-  export default Contact;
-  
+  return (
+    <div className="contact-container">
+      <h1>Contact Info</h1>
+      <p>Reach us at: icebergai@titanic.com</p>
+    </div>
+  );
+}
+
+export default Contact;
--- src/pages/Courses/Courses.jsx
+++ src/pages/Courses/Courses.jsx
@@ -1,5 +1,14 @@
+import "./Courses.css";
+
 function Courses() {
-  return <div>Courses Page</div>;
+  return (
+    <div className="courses-container">
+      <h1>Courses</h1>
+      <p>
+        Our upcoming course: "AI-Powered Web Applications with Titanic Data".
+      </p>
+    </div>
+  );
 }
 
 export default Courses;
