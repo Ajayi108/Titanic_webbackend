739eba7ffd6e0900680b687c458ef64c69fbb236
ya10410 <youssef.abdellatif@stud.th-deg.de>
2025-06-17 23:14:18 +0200

    adjust the abouy page css to allign with rest of the website

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,7 +2,7 @@ import "./About.css";
 
 function About() {
   return (
-    <div className="page-container">
+    <div className="page-container" >
       <h1>Welcome to IcebergAI</h1>
 
       <section className="section">
