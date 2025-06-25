1024535420e8f971c8c5a6c5c3354f0375748e18
Ikponmwosa Ajayi <ikponmwosa.ajayi@stud.th-deg.de>
2025-06-25 15:44:10 +0200

    fix error and reconnect with docker environment

--- routes/auth.py
+++ routes/auth.py
@@ -24,11 +24,11 @@ def get_config():
 
 # Database connection (hardcoded)
 conn = psycopg2.connect(
-    dbname="titanic_shrank_db",
-    user="titanic_saver",
-    password="TitanicMan",
-    host="localhost",
-    port="5432"
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST", "localhost"),
+    port=os.getenv("DB_PORT", "5432")
 )
 cursor = conn.cursor()
 
